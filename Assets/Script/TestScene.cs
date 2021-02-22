73d6c6d02d9f04596952597df80a52e89e54b64e
Lee0508 <liqiang497232807@gmail.com>
2021-02-22 17:49:36 +0800

    json 解析库

---
+++ Assets/Script/TestScene.cs
@@ -0,0 +1,65 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+public class TestScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("TestScene===Start");
+
+        var btn1 = GameObject.Find("btn1");
+
+        //按钮点击事件
+        btn1.GetComponent<Button>().onClick.AddListener(delegate() {
+
+            Debug.Log("66");
+            //GetRequest();
+            StartCoroutine(GetRequest());
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator GetRequest()
+    {
+        // 请求的 url
+        string url = "http://lee.free.vipnps.vip/hotupversion/configrelease";//你要请求的url地址
+        // 是什么请求就调用什么方法，如果是 post/put 方法，还需要传递一个 string 类型的数据
+        // 可以用 JSON 工具类将对象封装成 JSON
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+
+            yield return webRequest.SendWebRequest();
+
+            // 出现网络错误
+            if (webRequest.isHttpError || webRequest.isNetworkError)
+            {
+                Debug.LogError(webRequest.error + "\n" + webRequest.downloadHandler.text);
+            }
+            else// 正常处理
+            {
+                // 获取到的数据
+                string jsonText = webRequest.downloadHandler.text;
+                //Debug.Log(jsonText);
+
+
+                //json 解析
+                JObject obj = JObject.Parse(jsonText);
+                Debug.Log(obj["scriptVersion"]);
+
+            }
+        }
+    }
+}
